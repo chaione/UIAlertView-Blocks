c3dec10ef97044fe626b6ca0db9ffba80b868010
Yuichi Fujiki <yuichi@chaione.com>
2017-04-28 17:50:51 -0500

    Support Carthage.

---
+++ Tests/UIAlertView_Blocks/UIAlertView_Blocks.h
@@ -0,0 +1,19 @@
+//
+//  UIAlertView_Blocks.h
+//  UIAlertView_Blocks
+//
+//  Created by Yuichi Fujiki on 4/28/17.
+//  Copyright © 2017 Ryan Maxwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIAlertView_Blocks.
+FOUNDATION_EXPORT double UIAlertView_BlocksVersionNumber;
+
+//! Project version string for UIAlertView_Blocks.
+FOUNDATION_EXPORT const unsigned char UIAlertView_BlocksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIAlertView_Blocks/PublicHeader.h>
+
+
