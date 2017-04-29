b7c8cfb0c54843caa2700b82d89c0370bc007b5e
Yuichi Fujiki <yuichi@chaione.com>
2017-04-29 01:16:57 -0500

    Refer UIAlertView+Blocks.h from umbrella header.

--- Tests/UIAlertView_Blocks/UIAlertView_Blocks.h
+++ Tests/UIAlertView_Blocks/UIAlertView_Blocks.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char UIAlertView_BlocksVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UIAlertView_Blocks/PublicHeader.h>
 
+#import "UIAlertView+Blocks.h"
 
